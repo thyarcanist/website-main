a2b797e99a5e3ebe7c3b5c54b63e6ea1b2020d5c
voidespy <48234494+voidespy@users.noreply.github.com>
2022-10-02 19:53:01 -0400

    woop

---
+++ js/slideshow.js
@@ -0,0 +1,27 @@
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// directions
+function plusSlides(n)
+{
+    SlideShow(slidePosition += n);
+}
+
+// image controls
+function currentSlide(n) {
+    SlideShow(slidePosition - n);
+}
+
+function SlideShow(n) {
+    var i;
+    var slides = document.getElementsByClassName("containers");
+    var circles = document.getElementsByClassName("dots");
+    if (n > slides.length) {slidePosition = 1}
+    if (n < 1) {slidePosition = slides.length}
+    for (i = 0; i < circles.length; i++) {
+        circles[i].className = circles[i].className.replace(" enable", "");
+    }
+
+    slides[slidePosition-1].getElementsByClassName.display = block;
+    circles[slidePosition-1].className += " enable";
+}
